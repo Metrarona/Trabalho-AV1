e36920beffe92725cbe14ebb9778c2f0567dd98c
Metrarona <37021597@sempreunijuazeiro.com.br>
2024-04-12 14:18:16 -0300

    Arquivos

--- js/querySelector.js
+++ js/querySelector.js
@@ -1,4 +1,4 @@
-export var numbers = document.querySelectorAll(".numbers div");
-export var operations = document.querySelectorAll(".operations div");
-export var display = document.querySelector(".display");
+export var Numeros_Alfa = document.querySelectorAll(".Numeros_Alfa div");
+export var Operaçoes_India = document.querySelectorAll(".Operaçoes_India div");
+export var display_Tango = document.querySelector(".display_Tango");
 
