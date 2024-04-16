5b270a8e53102fde3b38108d95511fdcec485b01
Metrarona <37021597@sempreunijuazeiro.com.br>
2024-04-15 21:07:35 -0300

    Atualização

--- js/querySelector.js
+++ js/querySelector.js
@@ -1,4 +1,4 @@
-export var Numeros_Alfa = document.querySelectorAll(".Numeros_Alfa div");
-export var Operaçoes_India = document.querySelectorAll(".Operaçoes_India div");
-export var display_Tango = document.querySelector(".display_Tango");
+export var numbers = document.querySelectorAll(".numbers_november div");
+export var operations = document.querySelectorAll(".operations_oscar .oscar_op");
+export var display = document.querySelector(".display_delta");
 
